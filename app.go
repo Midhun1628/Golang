ecade6a29e4f05941320165b5d3ce4bbffc4c42e
Midhun <midhun@qawebprints.com>
2025-02-14 16:52:32 +0530

    showing database values in json format using gin

--- app.go
+++ app.go
@@ -1,20 +1,59 @@
 package main
 
-import ("database/sql"
-_"github.com/go-sql-driver/mysql"
-"fmt"
+import (
+	"fmt"
+	"log"
+	"database/sql"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	 "github.com/gin-gonic/gin"
 )
-func main(){
-	db,err:=sql.Open("mysql","root:qweasdzxc1@tcp(127.0.0.1:3306)/customer")
-   if err !=nil{
-		panic(err)
+
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:qweasdzxc1@tcp(127.0.0.1:3306)/customer")
+	if err != nil {
+		log.Fatal(err)
 	}
 	defer db.Close()
-	res,err:=db.Query ("update  users set name='Aski' where name='seee' ")
 
-if err !=nil{
-	panic(err)
-}
-	defer res.Close()
-fmt.Print("Mysql is connected and added the values")
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		rows, err := db.Query("SELECT id, name, age FROM users")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		defer rows.Close()
+
+		var users []User
+		for rows.Next() {
+			var user User
+			if err := rows.Scan(&user.ID, &user.Name, &user.Age); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			users = append(users, user)
+		}
+
+		
+		c.JSON(http.StatusOK, users)
+	})
+
+	fmt.Println("Server running on 3000")
+	r.Run(":3000")
 }
