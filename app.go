41f345f86a3a143fa5713bd4948b0c5d69a4cc19
Midhun <midhun@qawebprints.com>
2025-02-14 10:50:32 +0530

    connectng golang to mysql

--- app.go
+++ app.go
@@ -1,26 +1,20 @@
 package main
 
-import "fmt"
-
-type Personal struct{
-	name string
-	age int
-}
-
-var Per1 Personal
-var Per2 Personal
-
+import ("database/sql"
+_"github.com/go-sql-driver/mysql"
+"fmt"
+)
 func main(){
-	Per1.name="Midhun"
-	Per1.age=12
-	Per2.name="Midhunn"
-	Per2.age=13
+	db,err:=sql.Open("mysql","root:qweasdzxc1@tcp(127.0.0.1:3306)/customer")
+   if err !=nil{
+		panic(err)
+	}
+	defer db.Close()
+	res,err:=db.Query ("update  users set name='Aski' where name='seee' ")
 
-fmt.Print("Name : " ,Per1.name)
-fmt.Print(" Age : " ,Per1.age)
-fmt.Print(" Name :" ,Per2.name)
-fmt.Print("age : " ,Per2.age)
-
-
+if err !=nil{
+	panic(err)
 }
-
+	defer res.Close()
+fmt.Print("Mysql is connected and added the values")
+}
