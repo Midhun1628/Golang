f7fc1d13c80c1e0ee5f6b342d6c35849c11c8e51
Midhun <midhun@qawebprints.com>
2025-02-17 09:52:24 +0530

    Displaying database data in terminal without using gin(frame work. Commit before gin )

--- app.go
+++ app.go
@@ -1,59 +1,63 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"database/sql"
+	"fmt"
 	"net/http"
-
-	_ "github.com/go-sql-driver/mysql"
-	 "github.com/gin-gonic/gin"
+_ "github.com/go-sql-driver/mysql"
 )
 
 
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+func display(w http.ResponseWriter,r * http.Request){
+
+	
+	fmt.Fprint(w," data is sucessfully displayed on terminal")
 }
 
+
 func main() {
+	
+	// create a database object which can be used
+	// to connect with database.
+	db, err := sql.Open("mysql", "root:qweasdzxc1@tcp(0.0.0.0:3306)/customer")
+	
+if err !=nil{
+	panic(err)
+}
 
-	db, err := sql.Open("mysql", "root:qweasdzxc1@tcp(127.0.0.1:3306)/customer")
-	if err != nil {
-		log.Fatal(err)
+err = db.Ping()
+
+if err!=nil{
+	panic(err)
+}
+
+result,err:=db.Query("select * from users")
+
+if err !=nil{
+	panic(err)	
+}
+
+for result.Next(){
+
+	
+	var id int 
+	var name string
+	var age  int
+
+	err= result.Scan(&id,&name,&age)
+
+	if err !=nil{
+		panic(err)
 	}
-	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Database connection failed:", err)
-	}
+fmt.Printf("id: %d ,name: %s,age: %d \n",id,name,age )
+	
+	
+}
 
-	r := gin.Default()
+http.HandleFunc("/",display)
+fmt.Print("Server is running on 3000")
+http.ListenAndServe(":3000",nil)
 
-	r.GET("/", func(c *gin.Context) {
-		rows, err := db.Query("SELECT id, name, age FROM users")
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		defer rows.Close()
 
-		var users []User
-		for rows.Next() {
-			var user User
-			if err := rows.Scan(&user.ID, &user.Name, &user.Age); err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-			users = append(users, user)
-		}
-
-		
-		c.JSON(http.StatusOK, users)
-	})
-
-	fmt.Println("Server running on 3000")
-	r.Run(":3000")
 }
